f198067330bcc5afffc40fd9d02111710aa0ffda
Daksh <upadhyaydaksh96@gmail.com>
2021-11-18 01:41:14 -0500

    Added Splash Screen.

---
+++ app/src/main/java/com/gc/craftyme/SplashScreenActivity.kt
@@ -0,0 +1,21 @@
+package com.gc.craftyme
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val splashScreenTimeout: Long = 2500
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, splashScreenTimeout)
+    }
+}
