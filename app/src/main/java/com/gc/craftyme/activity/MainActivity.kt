a6641179478e85f29b1a064907e49be60b8fd9a0
Daksh <upadhyaydaksh96@gmail.com>
2021-11-28 13:02:20 -0500

    Added Firebase dependency to the project.

--- app/src/main/java/com/gc/craftyme/activity/MainActivity.kt
+++ app/src/main/java/com/gc/craftyme/activity/MainActivity.kt
@@ -3,8 +3,9 @@ package com.gc.craftyme.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gc.craftyme.R
+import com.gc.craftyme.helpers.DUBaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : DUBaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/gc/craftyme/helpers/DUBaseActivity.kt
@@ -0,0 +1,7 @@
+package com.gc.craftyme.helpers
+
+import androidx.appcompat.app.AppCompatActivity
+
+open class DUBaseActivity : AppCompatActivity() {
+
+}
