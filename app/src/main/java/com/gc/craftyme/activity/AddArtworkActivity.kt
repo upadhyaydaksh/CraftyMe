b7d50d76af044dcf91384da787fea5ae438e23d3
Daksh <upadhyaydaksh96@gmail.com>
2021-12-02 01:19:29 -0500

    Added Add Artwork activity & working on design.

---
+++ app/src/main/java/com/gc/craftyme/activity/AddArtworkActivity.kt
@@ -0,0 +1,12 @@
+package com.gc.craftyme.activity
+
+import android.os.Bundle
+import com.gc.craftyme.R
+import com.gc.craftyme.helpers.DUBaseActivity
+
+class AddArtworkActivity : DUBaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_artwork)
+    }
+}
