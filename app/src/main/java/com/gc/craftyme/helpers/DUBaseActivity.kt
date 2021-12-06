3747e88508eef1d3a78804a057cffc2985ef7ec1
Daksh <upadhyaydaksh96@gmail.com>
2021-12-05 22:58:22 -0500

    Handle signout.

--- app/src/main/java/com/gc/craftyme/helpers/DUBaseActivity.kt
+++ app/src/main/java/com/gc/craftyme/helpers/DUBaseActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.gc.craftyme.activity.HomeActivity
+import com.gc.craftyme.activity.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
 
 open class DUBaseActivity : AppCompatActivity() {
@@ -27,13 +29,17 @@ open class DUBaseActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    fun goToNextActivityWithoutHistory(activity: Class<*>?){
+        val intent = Intent(this, activity)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+    }
+
     fun signOut(){
-//        FirebaseAuth.getInstance().signOut()
-//        val intent = Intent(this, LoginActivity::class.java)
-//        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-//        startActivity(intent)
-//        toast("Signed out")
-//        firebaseAuth.signOut()
+        FirebaseAuth.getInstance().signOut();
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
     }
 
 }
